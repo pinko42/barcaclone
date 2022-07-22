9008fcef7d27f0c66292a154810d5755f9763e2c
pinko42 <mohamed.abderrezague@gmail.com>
2022-07-22 16:45:24 +0400

    unused components

--- src/components/Match/Card.js
+++ src/components/Match/Card.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./Card.css";
-import equipo from "../../resc/img3.webp"
 
 const Card = () => {
   return <div className="match-card-container">view all <b>Results</b></div>;
